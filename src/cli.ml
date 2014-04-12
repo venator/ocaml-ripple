7b0eb0356094c4b6490356603a97598d7cc8bf3e
Vincent Bernardoff <vb@luminar.eu.org>
2014-04-12 11:23:18 +0200

    [cli] Fix prompt display

--- src/cli.ml
+++ src/cli.ml
@@ -15,33 +15,34 @@ let print_commands () =
 
 (* Ripple sender *)
 
-let rec prompt ~push =
-  let send_command cmd =
-    push cmd;
-    prompt ~push
-  in
+let rec prompt mvar ~push =
+  Lwt_mvar.take mvar >>= fun () ->
   Lwt_io.print "> " >>= fun () ->
   Lwt_io.read_line Lwt_io.stdin >>= fun cmd ->
     try
-      send_command (List.assoc cmd commands)
+      push (List.assoc cmd commands);
+      prompt mvar ~push
     with
     | Not_found ->
         print_commands () >>= fun () ->
-        prompt ~push
+        Lwt_mvar.put mvar () >>= fun () ->
+        prompt mvar ~push
 
 (* Ripple receiver *)
 
-let rec response_handler ~stream ~push =
+let rec response_handler mvar ~stream ~push =
   Lwt_stream.next stream >>= fun response ->
   (* TODO: get response instead of string, see [ripple.ml] *)
   (* Lwt_io.printl (Ripple_api_j.string_of_response response) >>= *)
   Lwt_io.printl response >>= fun () ->
-  response_handler ~stream ~push
+  Lwt_mvar.put mvar () >>= fun () ->
+  response_handler mvar ~stream ~push
 
 (* Ripple handler *)
 
 let ripple_handler (stream, push) =
-  prompt ~push <&> response_handler ~stream ~push
+  let mvar = Lwt_mvar.create () in
+  prompt mvar ~push <&> response_handler mvar ~stream ~push
 
 (* Main *)
 
